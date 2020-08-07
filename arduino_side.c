28c6529d3a0e27fde7f25055c18b4cfd763e5bd3
toni7891 <toni7891@gmail.com>
2020-08-07 14:55:40 +0300

    chekc + rainbow wave - done

--- arduino_side.c
+++ arduino_side.c
@@ -1,23 +1,23 @@
 /*
 coded by:
 
-         @@@  @@@@@@@   @@@@@@@ @@@  @@@ @@@@@@  @@@@@@@   
-        @@@@  @@@@@@@@ @@@@@@@@ @@@  @@@ @@@@@@@ @@@@@@@@  
-       @@!@!  @@!  @@@ !@@      @@!  @@@     @@@ @@!  @@@  
-      !@!!@!  !@!  @!@ !@!      !@!  @!@     @!@ !@!  @!@  
-     @!! @!!  @!@!!@!  !@!      @!@!@!@! @!@!!@  @!@!!@!   
-    !!!  !@!  !!@!@!   !!!      !!!@!!!! !!@!@!  !!@!@!    
-    :!!:!:!!: !!: :!!  :!!      !!:  !!!     !!: !!: :!!   
-    !:::!!::: :!:  !:! :!:      :!:  !:!     :!: :!:  !:!  
-         :::  ::   :::  ::: ::: ::   ::: :: :::: ::   :::  
+         @@@  @@@@@@@   @@@@@@@ @@@  @@@ @@@@@@  @@@@@@@
+        @@@@  @@@@@@@@ @@@@@@@@ @@@  @@@ @@@@@@@ @@@@@@@@
+       @@!@!  @@!  @@@ !@@      @@!  @@@     @@@ @@!  @@@
+      !@!!@!  !@!  @!@ !@!      !@!  @!@     @!@ !@!  @!@
+     @!! @!!  @!@!!@!  !@!      @!@!@!@! @!@!!@  @!@!!@!
+    !!!  !@!  !!@!@!   !!!      !!!@!!!! !!@!@!  !!@!@!
+    :!!:!:!!: !!: :!!  :!!      !!:  !!!     !!: !!: :!!
+    !:::!!::: :!:  !:! :!:      :!:  !:!     :!: :!:  !:!
+         :::  ::   :::  ::: ::: ::   ::: :: :::: ::   :::
          :::   :   : :  :: :: :  :   : :  : : :   :   : :
 
 */
 
 #include <FastLED.h>
 
-#define NUM_LEDS 301      // Number of LED's in the strip [ 5 meter * 60 LED's per meter = 300 LED's in the whole strip  ]
-#define DATA_PIN 3        // Change later when arduino arrives      
+#define NUM_LEDS 300      // Number of LED's in the strip [ 5 meter * 60 LED's per meter = 300 LED's in the whole strip  ]
+#define DATA_PIN 6         // Change later when arduino arrives      
 #define LED_TYPE WS2812B  // Type of the LED strip
 #define BRIGHTNESS 255    // MIN [0 -> 255] MAX
 #define SATURATION 255    // MIN [0 -> 255] MAX
@@ -35,12 +35,32 @@ void setup()
     FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
 }
 
+void loop()
+{
+    leds[1] = 
+    rainbowWaveLoop();
+
+}
+
+/*
+check LED strip fully working [each led works as needed!]
+*/
+void check()
+{
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
+        leds[i] = CHSV(20, BRIGHTNESS, SATURATION);
+    }
+    FastLED.show();
+    delay(25)
+}
+
+
 /*
 loop for main event "RAINBOW WAVE"
 input: parameter's of the LED strip and the color settings
 output: NOW color grade and parameter's of the color 
 */
-
 void rainbowWaveLoop()
 {
     // Cycle hue
